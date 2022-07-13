f2834e47ecb184d41c9bf1c68b8d2b6b40582b38
Kapil Thapa <kapil.thapa@towercompanies.com>
2022-07-13 16:58:02 -0400

    Clarifai API

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Particle from './components/Particles/Particles';
+import Clarifai from 'clarifai';
 import './App.css';
+
+// const app = new Clarifai.App({
+//   apiKey: 'b691f6d8671d495eb1e0083d878e3af7'
+// });
 
 class App extends React.Component {
   constructor(props) {
@@ -20,6 +26,14 @@ class App extends React.Component {
 
   onButtonSubmit = () => {
     console.log('click');
+    // app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+    //   function(response) {
+    //     console.log(response);
+    //   },
+    //   function(err) {
+
+    //   }
+    // );
   }
 
   render() {    
@@ -33,7 +47,7 @@ class App extends React.Component {
           onInputChange={this.onInputChange}
           onButtonSubmit={this.onButtonSubmit}
         />
-        {/* <FaceRecognition /> */}
+        <FaceRecognition />
       </div>
     );
   }
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,7 +6,9 @@ const Logo = () => {
     return (
         <div className='ma4 mt0'>
             <Tilt className="Tilt br2 shadow-2" tiltMaxAngleX={40} tiltMaxAngleY={40} style={{ height: 150, width: 150 }} >
-                <div className="Tilt-inner pa3"><img style={{ paddingTop: '5px' }} src={brain} alt='logo' /></div>
+                <div className="Tilt-inner pa3">
+                    <img style={{ paddingTop: '5px' }} src={brain} alt='logo' />
+                </div>
             </Tilt>
         </div>
     );
