b1c0a3da9214d38d6ba166aff78279bae8bda32d
Kapil Thapa <kapildthapa@hotmail.com>
2022-07-21 01:25:29 -0400

    FE completion

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -2,7 +2,7 @@ const Register = ({ onRouteChange }) => {
     return (
         <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
             <main className="pa4 black-80">
-                <form className="measure">
+                <div className="measure">
                     <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
                     <legend className="f1 fw6 ph0 mh0">Registser</legend>
                     <div className="mt3">
@@ -26,7 +26,7 @@ const Register = ({ onRouteChange }) => {
                         value="Register"
                     />
                     </div>
-                </form>
+                </div>
             </main>
         </article>
     );
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -4,7 +4,7 @@ const Signin = ({ onRouteChange }) => {
     return (
         <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
             <main className="pa4 black-80">
-                <form className="measure">
+                <div className="measure">
                     <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
                     <legend className="f1 fw6 ph0 mh0">Sign In</legend>
                     <div className="mt3">
@@ -29,7 +29,7 @@ const Signin = ({ onRouteChange }) => {
                     <p onClick={() => onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
                     {/* <a href="#0" className="f6 link dim black db">Forgot your password?</a> */}
                     </div>
-                </form>
+                </div>
             </main>
         </article>
     );
